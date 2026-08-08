99f66b75c169962a913968af6f751db2dca362c4
Vijendra Rana <130141293+vkrana-me@users.noreply.github.com>
2026-08-08 20:45:07 +0530

    added index file

---
+++ index.js
@@ -0,0 +1,12 @@
+//simple app which listen on port 3000 and returns hello world
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
